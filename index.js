4ad6b204dade253c1aeb320980ffb2b72887efd5
yinkanglong <yinkanglong@163.com>
2020-11-27 14:44:42 +0800

    动态图

--- index.js
+++ index.js
@@ -23,10 +23,13 @@ sm2 = require('./build/Release/sm2');
 * 4. 打包发布。
 */
 $(document).ready(function(){
+    dis_none();
+
     $('#init_server').click(function(){
         result = sm2.init_server()
         $("#server_bar").attr("style","width:100%")
         // $('#modal').modal('show');
+        dis_all();
     })
 
     $('#a_gen_key').click(function(){
@@ -36,6 +39,8 @@ $(document).ready(function(){
         result = sm2.generate_A(private_key);
         // console.log(public_key.public_A)
         $('#a_public_key').val(result.public_A);
+        dis_none();
+        dis_interface_a();
         return;
     })
 
@@ -57,18 +62,27 @@ $(document).ready(function(){
             console.log(222)
         }
         $('#a_sign_time').text(result.time_signcrytion);
+        dis_none();
+        dis_interface_a();
         return;
     })
 
     $('#a_send').click(function(){
         message.send_message = message.a_sign_message;
-        sm2.send_signal_A()
+        sm2.send_signal_A();
+        dis_none();
+        dis_interface_a();
+        dis_a_b();
+        return;
     })
 
     $('#b_gen_key').click(function(){
         private_key = $('#b_private_key').val();
         result = sm2.generate_B(private_key);
         $('#b_public_key').val(result.public_B);
+        dis_none();
+        dis_interface_b();
+        return;
     })
 
     $('#b_receive').click(function(){
@@ -76,6 +90,9 @@ $(document).ready(function(){
         var receive_result = sm2.receive_signal_B();
         message.receive_message=receive_result.ciphertext_B;
         $('#b_sign_message').val(message.receive_message);
+        dis_none();
+        dis_interface_b();
+        return;
     })
 
     $('#b_unsign').click(function(){
@@ -113,7 +130,8 @@ $(document).ready(function(){
             $('#b_is_replay').text('否');
 
         }
-
+        dis_none();
+        dis_interface_b();
         return;
     })
 
@@ -126,7 +144,9 @@ $(document).ready(function(){
         else{
             $('#c_sign_message').val('failed to intercept')
         }
-
+        dis_none();
+        dis_interface_attacker();
+        dis_a_attacker();
         return;
     })
 
@@ -139,29 +159,34 @@ $(document).ready(function(){
             $('#c_tamper_message').val('failed to tamper');
         }
         message.tamper_message = result.ciphertext_new;
+        dis_none();
+        dis_interface_attacker();
+        dis_attacker_b();
         return ;
     })
 
-    $('#c_send').click(function(){
-        message.send_message = message.tamper_message;
-        return ;
-    })
+    // $('#c_send').click(function(){
+    //     message.send_message = message.tamper_message;
+    //     return ;
+    // })
 
-    $('#d_intercept').click(function(){
-        message.d_sign_message=message.send_message;
-        var result = sm2.intercept_cipher(message.send_message);
-        if(result.flag_intercept==1){
-            $('#d_sign_message').val(message.d_sign_message);
-        }
-        else{
-            $('#d_sign_message').val('failed to intercept')
-        }
+    // $('#d_intercept').click(function(){
+    //     message.d_sign_message=message.send_message;
+    //     var result = sm2.intercept_cipher(message.send_message);
+    //     if(result.flag_intercept==1){
+    //         $('#d_sign_message').val(message.d_sign_message);
+    //     }
+    //     else{
+    //         $('#d_sign_message').val('failed to intercept')
+    //     }
+    //     dis_none();
+    //     dis_interface_attacker();
+    //     dis_a_attacker();
+    //     return;
 
-        return;
+    // })
 
-    })
-
-    $('#d_send').click(function(){
+    $('#c_replay').click(function(){
         var result = sm2.replay_attack(message.d_sign_message);
         if(result.flag_do_replay==1){
             $('#d_replay_message').val(result.ciphertext_unsigncrytion);
@@ -171,15 +196,75 @@ $(document).ready(function(){
         }
         message.replay_message = result.ciphertext_unsigncrytion;
         message.send_message = message.replay_message;
+        dis_none();
+        dis_interface_attacker();
+        dis_attacker_b();
         return ;
     })
-
 })
 
 // 可以在在下边制作一组动画函数。
 // 使用HTML规范位置
 // 使用CSS3画图
 // 使用jQuery实现动画效果函数，然后在执行通信过程中，对通信过程进行动态展示
+
+function dis_none(){
+    $('#ib').hide();
+    $('#bi').hide();
+    $('#ia').hide();
+    $('#ai').hide();
+    $('#it').hide();
+    $('#ti').hide();
+    $('#ab').hide();
+    $('#ba').hide();
+    $('#at').hide();
+    $('#tb').hide();
+}
+function dis_all(){
+    $('#ib').fadeIn("slow");
+    $('#bi').fadeIn("slow");
+    $('#ia').fadeIn("slow");
+    $('#ai').fadeIn("slow");
+    $('#it').fadeIn("slow");
+    $('#ti').fadeIn("slow");
+    $('#ab').fadeIn("slow");
+    $('#ba').fadeIn("slow");
+    $('#at').fadeIn("slow");
+    $('#tb').fadeIn("slow");
+}
+function dis_interface_a(){
+    $('#ia').fadeIn("slow");
+    $('#ia').children().css("stroke","red");
+    $('#ai').fadeIn("slow");
+    $('#ai').children().css("stroke","red");
+
+}
+function dis_interface_b(){
+    $('#ib').fadeIn("slow");
+    $('#ib').children().css("stroke","red");
+    $('#bi').fadeIn("slow");
+    $('#bi').children().css("stroke","red");
+}
+function dis_interface_attacker(){
+    $('#it').fadeIn("slow");
+    $('#it').children().css("stroke","red");
+    $('#ti').fadeIn("slow");
+    $('#ti').children().css("stroke","red");
+}
+function dis_a_b(){
+    $('#ab').fadeIn("slow");
+    $('#ab').children().css("stroke","red");
+    $('#ba').fadeIn("slow");
+    $('#ba').children().css("stroke","red");
+}
+function dis_a_attacker(){
+    $('#at').fadeIn("slow");
+    $('#at').children().css("stroke","red");
+}
+function dis_attacker_b(){
+    $('#tb').fadeIn("slow");
+    $('#tb').children().css("stroke","red");
+}
 
 
 // document.getElementById("a_gen_key").onclick=function(){
